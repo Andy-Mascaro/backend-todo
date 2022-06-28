31865f3082102c7c07ac9370a2d20c774edf4985
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-27 19:15:49 -0700

    added test for create new client

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,28 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const fakeClient = {
+  email: 'fake@fake.com',
+  password: '111111',
+};
+
+describe('clients', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should create new client,', async () => {
+    const resp = await (
+      await request(app).post('/api/v1/clients')
+    ).setEncoding(fakeClient);
+    const { email } = fakeClient;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
