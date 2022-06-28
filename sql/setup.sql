671d58fa8775ed91e363d8de062685b22ca249cc
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-27 18:58:35 -0700

    added sql data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS clients;
+
+CREATE TABLE clients (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    client_id BIGINT,
+    description NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT(false),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (client_id) REFERENCES clients(id)
+)
